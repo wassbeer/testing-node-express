148c329af1bf15b179a1a17b7e3c2f7fe33fde11
Thomas Wassenberg <tm.wassenberg@gmail.com>
2018-02-13 10:28:57 +0100

    Test unit returning users created after certain date

--- test/unit/controllers.index.test.js
+++ test/unit/controllers.index.test.js
@@ -1,27 +1,46 @@
 process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var should = chai.should();
+const chai = require('chai');
+const should = chai.should();
 
-var indexController = require('../../src/server/controllers/index');
+const usersController = require('../../src/server/controllers/users');
 
-describe('controllers : index', function() {
 
-  describe('sum()', function() {
-    it('should return a total', function(done) {
-      indexController.sum(1, 2, function(err, total) {
-        should.not.exist(err);
-        total.should.eql(3);
-        done();
-      });
-    });
-    it('should return an error', function(done) {
-      indexController.sum(1, 'test', function(err, total) {
-        should.not.exist(total);
-        err.should.eql('Something went wrong!');
-        done();
-      });
-    });
+
+describe('controllers : users', () => {
+
+  describe('filterByYear()', () => {
+    // add code here
+
+    const userArray = [
+  {
+    id: 1,
+    username: 'michael',
+    email: 'michael@mherman.org',
+    created_at: '2016-09-10T16:44:28.015Z'
+  },
+  {
+    id: 2,
+    username: 'mike',
+    email: 'mike@mherman.org',
+    created_at: '2015-09-10T16:44:28.015Z'
+  },
+  {
+    id: 3,
+    username: 'mike',
+    email: 'mike@mherman.org',
+    created_at: '2014-09-10T16:44:28.015Z'
+  }
+];
+it('should return all users created on or after (>=) specified year',
+(done) => {
+  usersController.filterByYear(userArray, 2015, (err, total) => {
+    should.not.exist(err);
+    total.length.should.eql(2);
+    done();
+  });
+});
+
   });
 
 });
